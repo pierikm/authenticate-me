7f889af585eb4c92068d866acf4479753e618d9f
pierikm <pierikm@umich.edu>
2022-04-18 16:33:28 -0400

    stopping point

--- frontend/src/components/Reviews/Review.js
+++ frontend/src/components/Reviews/Review.js
@@ -1,7 +1,8 @@
 function Review({ review }) {
-    
+
     return (
         <>
+            <div>{review.User.username}</div>
             <div>{review.rating}</div>
             <div>{review.review}</div>
         </>
